60eb658521b456ff10de85b79f008d1b62ae125e
Thanuja Nipun <thanujanipun6@gmail.com>
2024-11-23 22:49:13 +0530

    First Commit, Java Data types add hello.java files

---
+++ Hello.java
@@ -0,0 +1,19 @@
+class Hello{
+    public static void main(String[] a){
+
+        int i = 5;
+
+        float f = 5.5f;
+
+        double d = 5.5; 
+        
+        char c = 'A';   
+    
+        boolean b = true;  
+
+        long l = 5000000000L;
+
+        short s = 5;
+    
+    }
+}
