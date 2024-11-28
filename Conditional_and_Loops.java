3460b93075231e2e4d7e08bb329405a045c2734f
Thanuja Nipun <thanujanipun6@gmail.com>
2024-11-29 00:04:24 +0530

    Add Switch statement in how using different version of java

--- Conditional_and_Loops.java
+++ Conditional_and_Loops.java
@@ -26,5 +26,58 @@ public class Conditional_and_Loops {
         
         result = (i%2 == 0) ? 1 : 0;
         System.out.println(result);
+
+        //Switch Case
+
+        int day = 3;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+            
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            default:
+                System.out.println("Enter Valid NO.");
+        }
+
+        //new version in using switch (after the 18 version)
+        String d = "Sunday";
+
+        switch (d) {
+            case "Saterday","Sunday" -> System.out.println("Wow it's weekends");        
+            case "Monday" -> System.out.println("ohh good it's weekday");
+            default -> System.out.println("ohh yah, it's playing days comming");
+        }
+
+        // also we can return the values 
+
+        String thanu = "tea";
+        String exp = "";
+
+        exp = switch (thanu)
+                {
+                    case "tea" : yield  "oh it's tea";
+                    case "coffiy" : yield "ewww it's coffiy";
+                    case "milk" : yield "ohh it's milk wow";
+                    default:
+                        throw new IllegalArgumentException("Unexpected value: " + thanu);
+        };
+
+        System.out.println(exp);
+
     }
 }
