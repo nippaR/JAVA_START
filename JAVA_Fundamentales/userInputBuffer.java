c4052609ad3601234976a33946b6ba530daeb17a
Thanuja Nipun <thanujanipun6@gmail.com>
2025-02-16 23:46:20 +0530

    added another way take user inputs using Buffer reader method

---
+++ JAVA_Fundamentales/userInputBuffer.java
@@ -0,0 +1,20 @@
+package JAVA_Fundamentales;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class userInputBuffer{
+    public static void main(String args[]) throws Exception
+    {
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        System.out.println("Enter your name: ");
+        String name = br.readLine();
+        System.out.println("Enter your age: ");
+        int age = Integer.parseInt(br.readLine());
+        System.out.println("");
+        System.out.println("Your name is: " + name);
+        System.out.println("Your age is: " + age);
+    }
+}
