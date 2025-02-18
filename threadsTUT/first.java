647c765c0a218f507cd37b9cfdd9a13a2e1f1084
Thanuja Nipun <thanujanipun6@gmail.com>
2025-02-18 23:21:59 +0530

    Modifiyed threads added priority threads and sleep time

--- threadsTUT/first.java
+++ threadsTUT/first.java
@@ -2,24 +2,42 @@ package threadsTUT;
 
 class A extends Thread{
    public void run(){
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 10; i++){
             System.out.println("Hello");
+            try{
+                Thread.sleep(10);
+            }catch(Exception e){
+                e.printStackTrace();
         }
     }
+}
 }
 
 class B extends Thread{
     public void run(){
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 10; i++){
             System.out.println("Hi");
+            try{
+                Thread.sleep(10);
+            }catch(Exception e){
+                e.printStackTrace();
         }
     }
+}
 }
 
 
 public class first {
     public static void main(String[] args) {
         A obj1 = new A();
+
+        obj1.setPriority(Thread.MAX_PRIORITY-1);
+
+        try{
+            Thread.sleep(2);   
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         B obj2 = new B();
 
 
