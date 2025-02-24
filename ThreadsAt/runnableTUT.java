40506ec5205be433cd66fe50ebca52d4b55179d8
Thanuja Nipun <thanujanipun6@gmail.com>
2025-02-24 23:45:04 +0530

    Added new branch as a reviews

---
+++ ThreadsAt/runnableTUT.java
@@ -0,0 +1,14 @@
+package ThreadsAt;
+
+class A implements Runnable{
+    public void run(){
+        for(int i = 0; i < 5; i++){
+            System.out.println("Hello");
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    };
+}   
